ded913b6cc2ca29fc169881f42a6469087dd383a
TOBI_DESKTOP\Tobih <tobihogh>
2023-11-27 19:56:47 +0100

    Implemented basic functions

--- Led.cpp
+++ Led.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h>
 #include "Led.h"
 
-void func(void)
+Led::Led(gpio_num_t gpioPin, bool invert) : pin(gpioPin), invert(invert)
 {
+    gpio_reset_pin(pin);
+    gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+    setState(OFF);
+}
 
+void Led::setInvert(bool invert)
+{
+    this->invert = invert;
+}
+
+void Led::setState(led_state state)
+{
+    if (invert) {
+        this->state = !state;
+    } else {
+        this->state = state;
+    }
+    gpio_set_level(pin, this->state);
+}
+
+void Led::toggle()
+{
+    state = !state;
+    gpio_set_level(pin, state);
 }
--- include/Led.h
+++ include/Led.h
@@ -1,1 +1,25 @@
-void func(void);
+#include "driver/gpio.h"
+
+
+
+class Led 
+{
+    typedef uint32_t led_state;
+
+    public: 
+        static const led_state OFF = 0;
+        static const led_state ON = 1;
+
+        Led(gpio_num_t pin, bool invert = true);
+
+        void setState(led_state state);
+        void toggle();
+        void setInvert(bool invert);
+
+    private:
+        gpio_num_t pin;
+
+        led_state state = OFF;
+        bool invert = true;
+    
+};
